e8e47af300b28f146031eb122aabe3af14deb599
Aya <90342033+Aya-X@users.noreply.github.com>
2023-09-01 10:02:00 +0800

    Updates

---
+++ assets/index-ae19f2f9.js
@@ -0,0 +1,1 @@
+import"./main-7d02749a.js";import e from"https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs";new e(".feedbackSwiper",{slidesPerView:"auto",scrollbar:{el:".swiper-scrollbar",draggable:!0},spaceBetween:24,mousewheel:!0});
